e79c97c8889368a9b4c420023df63a5a3452e695
BrahimBenzekri <benzbra04@gmail.com>
2025-03-09 16:05:43 +0100

    Fixed the bottom bar untouchable bug, and added animation to it

--- lib/features/navigation/widgets/animated_bottom_bar.dart
+++ lib/features/navigation/widgets/animated_bottom_bar.dart
@@ -14,53 +14,70 @@ class AnimatedBottomBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 30),
-        padding: const EdgeInsets.all(10),
-        height: 70,
-        width: MediaQueryHelper.getScreenWidth(context) - 60,
-        decoration: BoxDecoration(
-          color: AppColors.bottomBarColor,
-          borderRadius: BorderRadius.circular(35),
-        ),
-        child: Obx(
-          () => Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: navigationController.barItems
-                .map(
-                  (item) => GestureDetector(
-                    onTap: () {
-                      navigationController.changePage(
-                          navigationController.barItems.indexOf(item));
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SvgPicture.asset(
-                          navigationController.selectedIndex.value ==
-                                  navigationController.barItems.indexOf(item)
-                              ? item.activeIcon
-                              : item.inactiveIcon,
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.all(10),
+      height: 70,
+      width: MediaQueryHelper.getScreenWidth(context) - 60,
+      decoration: BoxDecoration(
+        color: AppColors.bottomBarColor,
+        borderRadius: BorderRadius.circular(35),
+      ),
+      child: Obx(
+        () => Row(
+          children: List.generate(
+            navigationController.barItems.length,
+            (index) => Expanded(
+              child: GestureDetector(
+                behavior:
+                    HitTestBehavior.opaque, // Makes the entire area touchable
+                onTap: () => navigationController.changePage(index),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedScale(
+                      duration: const Duration(milliseconds: 200),
+                      scale: navigationController.selectedIndex.value == index
+                          ? 1.2
+                          : 1.0,
+                      child: AnimatedOpacity(
+                        duration: const Duration(milliseconds: 200),
+                        opacity:
+                            navigationController.selectedIndex.value == index
+                                ? 1.0
+                                : 0.5,
+                        child: SvgPicture.asset(
+                          navigationController.selectedIndex.value == index
+                              ? navigationController.barItems[index].activeIcon
+                              : navigationController
+                                  .barItems[index].inactiveIcon,
                           height: 25,
                           width: 25,
                         ),
-                        const SizedBox(height: 5),
-                        Text(
-                          item.name,
-                          style: TextStyle(
-                            color: navigationController.selectedIndex.value ==
-                                    navigationController.barItems.indexOf(item)
-                                ? AppColors.textColorLight
-                                : AppColors.textColorInactive,
-                            fontSize: 10,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                )
-                .toList(),
+                    const SizedBox(height: 5),
+                    AnimatedDefaultTextStyle(
+                      duration: const Duration(milliseconds: 200),
+                      style: TextStyle(
+                        color: navigationController.selectedIndex.value == index
+                            ? AppColors.textColorLight
+                            : AppColors.textColorInactive,
+                        fontSize:
+                            navigationController.selectedIndex.value == index
+                                ? 12
+                                : 10,
+                        fontFamily: 'Oddval',
+                      ),
+                      child: Text(navigationController.barItems[index].name),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
