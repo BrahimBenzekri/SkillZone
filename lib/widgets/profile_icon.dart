d5863ad6810fd2416f145db3d46d4c2430c502a5
BrahimBenzekri <benzbra04@gmail.com>
2025-05-09 11:02:19 +0100

    Pressing the profile icon takes the user to the profile page now!

--- lib/widgets/profile_icon.dart
+++ lib/widgets/profile_icon.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:skillzone/core/theme/app_colors.dart';
+import 'package:skillzone/features/navigation/controllers/navigation_controller.dart';
 
 class ProfileIcon extends StatelessWidget {
 
@@ -14,20 +16,25 @@ class ProfileIcon extends StatelessWidget {
     final savedAvatarImage = storage.read<String>('avatar_image') ?? 'lib/assets/images/avatar13.png';
     final savedColorIndex = storage.read<int>('avatar_color_index') ?? 0;
 
-    return Container(
-        height: 30,
-        width: 30,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: AppColors.avatarColors[savedColorIndex].withValues(alpha: 0.2),
-          border: Border.all(
-            color: AppColors.avatarColors[savedColorIndex],
-            width: 2,
+    return GestureDetector(
+      onTap: () {
+        Get.find<NavigationController>().changePage(3);
+      },
+      child: Container(
+          height: 30,
+          width: 30,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppColors.avatarColors[savedColorIndex].withValues(alpha: 0.2),
+            border: Border.all(
+              color: AppColors.avatarColors[savedColorIndex],
+              width: 2,
+            ),
+          ),
+          child: Image.asset(
+           savedAvatarImage,
           ),
         ),
-        child: Image.asset(
-         savedAvatarImage,
-        ),
-      );
+    );
   }
 }
